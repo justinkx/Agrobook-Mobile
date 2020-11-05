0a5a15e9e195adbde4c8eb8cca06d935b299c314
Justin K Xavier <justinkizhakkeparampil@gmail.com>
2020-11-05 13:04:23 +0530

    added graphql state persist

--- App/App.tsx
+++ App/App.tsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import AppNavigator from "./Navigation/Navigation";
 import { NavigationContainer } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-import store, { persistor } from "./Store/Store";
-import { Provider } from "react-redux";
-import { PersistGate } from "redux-persist/integration/react";
 import { ApolloProvider } from "@apollo/client";
-import appoloClient from "./GraphQl/AppoloClient";
+import appoloClientInit from "./GraphQl/AppoloClient";
 
 export default function App() {
+  const [client, setClient] = useState<any>(undefined);
+  useEffect(() => {
+    async function init() {
+      const appoloClient = await appoloClientInit();
+      setClient(appoloClient);
+    }
+    init();
+  }, []);
   return (
     <>
       <StatusBar
@@ -19,13 +24,11 @@ export default function App() {
         style="dark"
       />
       <NavigationContainer>
-        <Provider store={store}>
-          <ApolloProvider client={appoloClient}>
-            <PersistGate persistor={persistor}>
-              <AppNavigator />
-            </PersistGate>
+        {client && (
+          <ApolloProvider client={client}>
+            <AppNavigator />
           </ApolloProvider>
-        </Provider>
+        )}
       </NavigationContainer>
     </>
   );
--- App/GraphQl/AppoloClient.tsx
+++ App/GraphQl/AppoloClient.tsx
@@ -1,8 +1,22 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { persistCache } from "apollo3-cache-persist";
+import FSStorage from "redux-persist-expo-fs-storage";
 
-const appoloClient = new ApolloClient({
-  uri: "https://agrobook-backoffice.herokuapp.com/graphql",
-  cache: new InMemoryCache(),
-});
+// const appoloClient = new ApolloClient({
+//   uri: "https://agrobook-backoffice.herokuapp.com/graphql",
+//   cache: new InMemoryCache(),
+// });
+async function appoloClientInit() {
+  const cache = new InMemoryCache();
+  const appoloClient = new ApolloClient({
+    uri: "https://agrobook-backoffice.herokuapp.com/graphql",
+    cache: cache,
+  });
+  await persistCache({
+    cache: cache,
+    storage: FSStorage(),
+  });
 
-export default appoloClient;
+  return appoloClient;
+}
+export default appoloClientInit;
